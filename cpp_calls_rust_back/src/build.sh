d6f791befe57d3c544f42da4e657a57194d5498c
Lucas Zanella <me@lucaszanella.com>
2021-01-21 04:09:08 +0000

    c++ compiles

--- cpp_calls_rust_back/src/build.sh
+++ cpp_calls_rust_back/src/build.sh
@@ -1,3 +1,1 @@
-#rustc --crate-type=staticlib interface.rs
-g++ -o libsomeclass -L. someclass.cpp -pthread -ldl
-#rustc --crate-type=cdylib interface.rs
+g++ -c -o libsomeclass someclass.cpp -pthread -ldl
--- cpp_calls_rust_back/src/someclass.cpp
+++ cpp_calls_rust_back/src/someclass.cpp
@@ -1,22 +1,37 @@
 #include <iostream>
 #include <thread>
+#include <chrono>
 
-typedef int (*onDataCallback_)(uint8_t *data, size_t len);
+typedef void (*fptr)(int);
 
 class SomeClass {
+public: 
     SomeClass() {
 
     }
 
     void run() {
-        std::thread t([](){
-
-        }, "Hello");
+        std::thread t([this](){
+            std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+            fptr rust_callback = reinterpret_cast<fptr>(reinterpret_cast<long>(this->callback_in_rust)) ;
+            (rust_callback)(3);
+        });
         t.join();
     }
-}
 
+    void* callback_in_rust;
+};
 
 extern "C" void* cpp_new_some_class() {
     return new SomeClass();
+}
+
+//Sets the Rust function `callback_in_rust` to be called by this SomeClass `instance`
+extern "C" void* cpp_some_class_set_callback(void* instance, void* callback_in_rust) {
+    ((SomeClass*)instance)->callback_in_rust = callback_in_rust;
+}
+
+//Runs the `run` function which delays a bit before calling back the rust function
+extern "C" void* cpp_some_class_run(void* instance) {
+    ((SomeClass*)instance)->run();
 }
