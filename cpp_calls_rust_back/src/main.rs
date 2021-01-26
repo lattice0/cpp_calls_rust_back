0443c1a6293a7cd13619872f75bbdf535e4ca1ad
Lucas Zanella <me@lucaszanella.com>
2021-01-26 04:22:04 +0000

    read allocate WORKS!

--- cpp_calls_rust_back/src/main.rs
+++ cpp_calls_rust_back/src/main.rs
@@ -1,33 +1,45 @@
 use std::{
-    io::{Read, Write, Result},
-    os::raw::{c_char, c_int, c_void},
+    io::{Result},
 };
-
-//use std::sync::{Arc, Mutex};
+use libc::{c_int, c_void, size_t};
 
 struct OVPNClient {
     openvpn_client: *mut c_void,
 }
 
+//Just a struct used for implementing the read/write functions
 struct OVPNClientInner {
     
 }
 
-impl Read for OVPNClientInner {
+impl OVPNClientInner {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
         println!("read for buf with size {}", buf.len());
         Ok(0)
     }
-}
 
-impl Write for OVPNClientInner {
+    fn read_allocate(&mut self, buffer: *mut *mut u8) -> Result<usize> {
+        //println!("read for buf with size {}", buf.len());
+        println!("OVPNClientInner read_allocate");
+        let s:u8 = 10;
+        let b: *mut u8 = unsafe{openvpn_client_allocate(s as usize)};
+        println!("did allocation");
+        for i in 0..s {
+            unsafe{
+                *b.offset(i as isize) = i;
+            }
+        }
+        println!("finished setting buffer");
+        unsafe{
+            *buffer = b
+        };
+        println!("*buffer = b");
+        Ok(s as usize)
+    }
+
     fn write(&mut self, buf: &[u8]) -> Result<usize> {
         println!("write for buf with size {}", buf.len());
         Ok(0)
-    }
-    fn flush(&mut self) -> Result<()> {
-        println!("flush!");
-        Ok(())
     }
 }
 
@@ -38,8 +50,9 @@ impl OVPNClient {
         };
         let callbacks = Callbacks {
             user_data: Box::into_raw(Box::new(inner)) as *mut c_void,
-            on_read: on_read_trampoline::<OVPNClientInner>,
-            on_write: on_write_trampoline::<OVPNClientInner>,
+            on_read: on_read_trampoline,
+            on_read_allocate: on_read_allocate_trampoline,
+            on_write: on_write_trampoline,
             destroy: destroy_trampoline::<OVPNClientInner>,
         };
         OVPNClient {
@@ -50,6 +63,7 @@ impl OVPNClient {
     pub fn run(&self) -> i32 {
         unsafe{openvpn_client_run(self.openvpn_client)}
     }
+
 }
 
 impl Drop for OVPNClient {
@@ -73,6 +87,8 @@ extern "C" {
     fn openvpn_client_run(client: *mut OpenVpnClient) -> c_int;
     /// Destroy the OpenVPN client.
     fn openvpn_client_free(client: *mut OpenVpnClient);
+    /// Allocates, on C++, a uint8_t* buffer with size `size`
+    fn openvpn_client_allocate(size: size_t) -> *mut u8;
 }
 
 /// An opaque type representing the C++ OpenVPN client.
@@ -83,19 +99,22 @@ pub struct Callbacks {
     /// A pointer to some user-defined state.
     pub user_data: *mut c_void,
     /// Callback fired when the OpenVPN client wants to read data.
-    pub on_read: unsafe extern "C" fn(*mut c_char, c_int, *mut c_void) -> c_int,
+    pub on_read: unsafe extern "C" fn(*mut u8, size_t, *mut c_void) -> c_int,
+    /// Callback fired when the OpenVPN client wants to read data but does not know the data size
+    /// so it leaves to Rust the task of allocating. Returns 0 on success, -1 on failure
+    pub on_read_allocate: unsafe extern "C" fn(*mut *mut u8, *mut size_t, *mut c_void) -> c_int,
     /// Callback fired when the OpenVPN client wants to write some data.
-    pub on_write: unsafe extern "C" fn(*const c_char, c_int, *mut c_void) -> c_int,
+    pub on_write: unsafe extern "C" fn(*const u8, size_t, *mut c_void) -> c_int,
     /// A function for destroying the user-defined state.
     pub destroy: unsafe extern "C" fn(*mut c_void),
 }
 
-unsafe extern "C" fn on_read_trampoline<P: Read>(
-    buffer: *mut c_char,
-    len: c_int,
+unsafe extern "C" fn on_read_trampoline(
+    buffer: *mut u8,
+    len: size_t,
     user_data: *mut c_void,
 ) -> c_int {
-    let user_data = &mut *(user_data as *mut P);
+    let user_data = &mut *(user_data as *mut OVPNClientInner);
     let buffer = std::slice::from_raw_parts_mut(buffer as *mut u8, len as usize);
 
     match user_data.read(buffer) {
@@ -104,12 +123,34 @@ unsafe extern "C" fn on_read_trampoline<P: Read>(
     }
 }
 
-unsafe extern "C" fn on_write_trampoline<P: Write>(
-    buffer: *const c_char,
-    len: c_int,
+unsafe extern "C" fn on_read_allocate_trampoline(
+    buffer: *mut *mut u8,
+    len: *mut size_t,
     user_data: *mut c_void,
 ) -> c_int {
-    let user_data = &mut *(user_data as *mut P);
+    println!("on_read_allocate_trampoline");
+    let user_data = &mut *(user_data as *mut OVPNClientInner);
+    //let buffer = std::slice::from_raw_parts_mut(buffer as *mut u8, len as usize);
+    match user_data.read_allocate(buffer) {
+        Ok(allocated_size) => {
+            println!("Ok(allocated_size) for allocated_size: {}", allocated_size);
+            *len = allocated_size;
+            println!("returning from on_read_allocate_trampoline");
+            0 as c_int
+        },
+        Err(_) => {
+            println!("err from read_allocate");
+            -1
+        },
+    }
+}
+
+unsafe extern "C" fn on_write_trampoline(
+    buffer: *const u8,
+    len: size_t,
+    user_data: *mut c_void,
+) -> c_int {
+    let user_data = &mut *(user_data as *mut OVPNClientInner);
     let buffer = std::slice::from_raw_parts(buffer as *const u8, len as usize);
 
     match user_data.write(buffer) {
--- cpp_calls_rust_back/src/openvpnclient.cpp
+++ cpp_calls_rust_back/src/openvpnclient.cpp
@@ -3,8 +3,9 @@
 
 struct Callbacks {
   void *user_data;
-  int (*on_read)(char *buffer, int len, void *user_data);
-  int (*on_write)(const char *buffer, int len, void *user_data);
+  int (*on_read)(uint8_t *buffer, int size_t, void *user_data);
+  int (*on_read_allocate)(uint8_t **buffer, size_t* size, void *user_data);
+  int (*on_write)(const uint8_t *buffer, int size_t, void *user_data);
   void (*destroy)(void *user_data);
 };
 
@@ -35,23 +36,40 @@ public:
 };
 
 extern "C" {
-OpenVpnClient *openvpn_client_new(Callbacks callbacks) {
-  return new OpenVpnClient(callbacks);
-}
+  OpenVpnClient *openvpn_client_new(Callbacks callbacks) {
+    return new OpenVpnClient(callbacks);
+  }
 
-int openvpn_client_run(OpenVpnClient *client) {
-  try {
-    // do stuff, calling client.callbacks.on_read and friends when
-    // things happen.
-    char* b;
-    int r = client->callbacks.on_read(b, 5, client->callbacks.user_data);
-    return 0;
-  } catch (std::exception &e) {
-    return -1;
+  int openvpn_client_run(OpenVpnClient *client) {
+    try {
+      // do stuff, calling client.callbacks.on_read and friends when
+      // things happen.
+      uint8_t* b;
+      size_t size;
+      std::cout << "gonna on_read_allocate" << std::endl;
+      int r = client->callbacks.on_read_allocate(&b, &size, client->callbacks.user_data);
+      std::cout << "DID on_read_allocate" << std::endl;
+      if (r!=0) {
+        throw std::invalid_argument("error on on_read_allocate");
+      }
+      std::cout << "received array: " << std::endl;
+      for (size_t i=0; i<size; i++) {
+        std::cout << (int) b[i];
+      }
+      std::cout << std::endl;
+      delete[] b;
+      return 0;
+    } catch (std::exception &e) {
+      return -1;
+    }
+  }
+
+  void openvpn_client_free(OpenVpnClient *client) { 
+    delete client; 
+  }
+
+  uint8_t* openvpn_client_allocate(size_t size) {
+    std::cout << "c++ openvpn_client_allocate for size " << size << std::endl;
+    return new uint8_t[size]();
   }
 }
-
-void openvpn_client_free(OpenVpnClient *client) { delete client; }
-}
-
-//int main() { return 0; }
